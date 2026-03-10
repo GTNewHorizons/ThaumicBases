bbd032bd41daa74963ff949596fdde1b8c836360
__felix__ <134875831+felixfour@users.noreply.github.com>
2026-03-10 17:40:38 -0400

    spotless

--- src/main/java/tb/common/tile/TileOverchanter.java
+++ src/main/java/tb/common/tile/TileOverchanter.java
@@ -248,8 +248,9 @@ public class TileOverchanter extends TileEntity implements ISidedInventory, IWan
 
     @Override
     public boolean isItemValidForSlot(int slot, ItemStack stk) {
-        return stk.hasTagCompound() && stk.getEnchantmentTagList() != null &&
-            stk.getEnchantmentTagList().tagCount() > 0 && findEnchantment(stk) != -1;
+        return stk.hasTagCompound() && stk.getEnchantmentTagList() != null
+            && stk.getEnchantmentTagList().tagCount() > 0
+            && findEnchantment(stk) != -1;
     }
 
     @Override
